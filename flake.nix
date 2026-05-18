4b7abbf052ed77b473bf42d227160e6ca9000aa1
leoporoli <leandroporoli@gmail.com>
2026-05-18 16:33:45 -0300

    changing username in flake

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       
       # ─── User Configuration ───
       # Change this to your macOS username
-      username = "leandroporoli";
+      username = "leandro.poroli";
 
       # Function to create home configuration for a specific system
       mkHomeConfiguration = system:
